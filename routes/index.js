5b9379a78fd5e3d4143c3f52af22bf64e212353a
ChachiQ <qiuqi830@gmail.com>
2018-06-13 10:32:01 +0800

    v0.0.1

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,42 @@
 var express = require('express');
+var config = require('../config');
 var router = express.Router();
+
+var WechatApi = require('co-wechat-api');
+var wechat = new WechatApi(config.wetchat.appid, config.wetchat.appSecret);
+var jsSHA = require('jssha');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  console.debug(req);
+  let signature = req.query.signature;
+  let timestamp = req.query.timestamp;
+  let echostr = req.query.echostr;
+  let nonce = req.query.nonce;
+  if (signature.length && timestamp.length && echostr.length) {
+    if (wechatSignVerify(signature, timestamp, nonce)) {
+      res.send(echostr);
+      return;
+    }
+  }
+  res.sendStatus(200);
 });
+
+router.get('/update_menu', async function (req, res, next) {
+  let result = await wechat.createMenu(config.gzhMenu);
+  console.debug(result);
+  res.status(200).send(result);
+})
+
+const wechatSignVerify = (signature, timestamp, nonce) => {
+  let arr = [nonce, timestamp, config.wetchat.token]
+  arr.sort();
+
+  let original = oriArray.join('');
+  let shaObj = new jsSHA(original, 'TEXT');
+  let scyptoString = shaObj.getHash('SHA-1', 'HEX');
+  console.debug(scyptoString + "  :   " + signature);
+  return signature.length && scyptoString === signature;
+}
 
 module.exports = router;
