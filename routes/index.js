50b0f5d53266b8c8073cd47ef5e09e8a2e4ae58e
ChachiQ <qiuqi830@gmail.com>
2018-06-13 14:52:46 +0800

    test..

--- routes/index.js
+++ routes/index.js
@@ -14,6 +14,7 @@ router.get('/', function(req, res, next) {
   let echostr = req.query.echostr;
   let nonce = req.query.nonce;
   if (signature && timestamp && echostr) {
+    console.log('adfasdf')
     if (wechatSignVerify(signature, timestamp, nonce)) {
       res.send(echostr);
       return;
