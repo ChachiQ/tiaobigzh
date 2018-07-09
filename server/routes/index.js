03b1281e4ff5a0b3ce11f8ebe470e5b54dc86feb
ChachiQ <qiuqi830@gmail.com>
2018-07-09 22:09:30 +0800

    s

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,11 +1,14 @@
 var express = require('express');
 var router = express.Router();
-var debug = require('debug')('app.index');
-var path = require('path');
 
 /* GET home page. */
 router.get('/', function(_, res) {
-  res.render('index');
+  try {
+    res.render('index');
+  } catch (err) {
+    console.log(err);
+    throw err;
+  }
 });
 
 module.exports = router;
